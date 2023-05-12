d2f98e379c0814f50c5663f51608cbc23877f84a
mhsong <mhsong@kookmin.ac.kr>
2023-05-13 01:04:35 +0900

    Fix error with yolov5 model download

--- models/model_download.sh
+++ models/model_download.sh
@@ -12,7 +12,8 @@ unzip -q inception_v3.zip && rm inception_v3.zip
 #object detection TF/FP32 model (yolo v5)
 curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/yolo_v5/yolo_v5.zip
 unzip -q yolo_v5.zip && rm yolo_v5.zip
-mv yolov5 yolo_v5
+mv yolov5/yolov5s_saved_model yolo_v5
+rm -rf yolov5
 
 #bert imdb model
 curl -O https://edge-inference.s3.us-west-2.amazonaws.com/NLP/bert_imdb.zip
