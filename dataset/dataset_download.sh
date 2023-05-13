f4f8992cc38c7f085fddd4340df3681a1f8d37eb
mhsong <mhsong@kookmin.ac.kr>
2023-05-14 01:52:32 +0900

    remove coco dataset download

--- dataset/dataset_download.sh
+++ dataset/dataset_download.sh
@@ -10,10 +10,3 @@ curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/i
 curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/imagenet_1000_raw.zip
 mv imagenet_metadata.txt ./imagenet
 unzip -q imagenet_1000_raw.zip -d ./imagenet && rm imagenet_1000_raw.zip
-
-#object detection image dataset(coco_2017)
-mkdir coco_2017
-curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/coco_2017/val_dataset.py
-python3 val_dataset.py
-mv val_dataset.py ./coco_2017
-unzip -q coco2017val.zip -d ./ && rm coco2017val.zip
