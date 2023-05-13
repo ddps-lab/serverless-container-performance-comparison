2c25371d8adf39bd00ed12c470249ec855d169d7
mhsong <mhsong@kookmin.ac.kr>
2023-05-14 01:23:13 +0900

    Fix typo

--- bench/bench_aws_infra/ec2.tf
+++ bench/bench_aws_infra/ec2.tf
@@ -51,21 +51,21 @@ resource "aws_instance" "bench_instance" {
     "Name" : "${var.prefix}-bench-instance"
   }
   user_data = <<-EOF
-    #!/bin/bash
-    apt update
-    apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-  libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-  xz-utils tk-dev liblzma-dev unzip curl docker.io
+  #!/bin/bash
+  apt update
+  apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev liblzma-dev unzip curl docker.io
 
-    echo 'export PYENV_ROOT="/pyenv"' >> /etc/bash.bashrc
-    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> /etc/bash.bashrc
-    echo 'eval "$(pyenv init -)"' >> /etc/bash.bashrc
+  echo 'export PYENV_ROOT="/pyenv"' >> /etc/bash.bashrc
+  echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> /etc/bash.bashrc
+  echo 'eval "$(pyenv init -)"' >> /etc/bash.bashrc
 
-    git clone https://github.com/pyenv/pyenv.git /pyenv
-    chmod 777 -R /pyenv
-    source /etc/bash.bashrc
+  git clone https://github.com/pyenv/pyenv.git /pyenv
+  chmod 777 -R /pyenv
+  source /etc/bash.bashrc
 
-    pyenv install 3.11.3
+  pyenv install 3.11.3
   EOF
   root_block_device {
     volume_size           = 64    # 볼륨 크기를 지정합니다.
