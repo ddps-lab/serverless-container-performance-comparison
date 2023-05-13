71c2aa98bacd3195ac8bf8d1d076f708915fcab1
mhsong <mhsong@kookmin.ac.kr>
2023-05-13 18:49:37 +0900

    Fix error about dataset path

--- bench/mobilenet_v2/rest_bench.py
+++ bench/mobilenet_v2/rest_bench.py
@@ -11,7 +11,7 @@ import concurrent.futures
 
 def run_bench(num_tasks, server_address):
     model_name = "mobilenet_v2"
-    image_file_path = "../../../dataset/imagenet/imagenet_1000_raw/n01843383_1.JPEG"
+    image_file_path = "../../dataset/imagenet/imagenet_1000_raw/n01843383_1.JPEG"
     
     data = json.dumps({"instances": preprocessing.run_preprocessing(image_file_path).tolist()})
 
