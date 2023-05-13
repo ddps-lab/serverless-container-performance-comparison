8876e3c7920a8e8be897725d16af5dfca7fa40f5
mhsong <mhsong@kookmin.ac.kr>
2023-05-13 19:26:05 +0900

    Change distilbert_sst2 preprocessing code and Add rest bench code

--- bench/distilbert_sst2/preprocessing.py
+++ bench/distilbert_sst2/preprocessing.py
@@ -4,8 +4,7 @@ import tensorflow as tf
 def run_preprocessing(text):
     tokenizer = tf.keras.preprocessing.text.Tokenizer()
     tokenizer.fit_on_texts([text])
-    input_ids = tokenizer.texts_to_sequences([text])
-    input_mask = [[1] * len(input_ids[0])]
-    segment_ids = [[0] * len(input_ids[0])]
+    bert_input_ids = tokenizer.texts_to_sequences([text])
+    bert_input_mask = [[1] * len(bert_input_ids[0])]
 
-    return input_ids, input_mask, segment_ids 
+    return bert_input_ids, bert_input_mask
---
+++ bench/distilbert_sst2/rest_bench.py
@@ -0,0 +1,29 @@
+#preprocessing library
+from distilbert_sst2 import preprocessing
+import numpy as np
+
+#REST 요청 관련 library
+from module import module_rest
+import json
+
+#병렬처리 library
+import concurrent.futures
+
+def run_bench(num_tasks, server_address):
+    model_name = "bert_imdb"
+    
+    text = "This is a sample sentence to test the BERT model."
+    bert_input_ids, bert_input_mask = preprocessing.run_preprocessing(text)
+
+    data = json.dumps({"inputs": { "bert_input_masks": bert_input_mask, "bert_input_ids": bert_input_ids}})
+
+    # REST 요청 병렬 처리
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_tasks) as executor:
+        futures = [executor.submit(lambda: module_rest.predict(server_address, model_name, data)) for _ in range(num_tasks)]
+
+    inference_times_include_network_latency = []
+    for future in concurrent.futures.as_completed(futures):
+        result, thread_elapsed_time = future.result()
+        inference_times_include_network_latency.append(thread_elapsed_time)
+
+    return inference_times_include_network_latency
