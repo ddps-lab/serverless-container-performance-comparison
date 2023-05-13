412a91b9ae2bb36db4f9d77bd6f9a9b0ddaf99a7
mhsong <mhsong@kookmin.ac.kr>
2023-05-13 22:18:49 +0900

    Test aws lambda docker image script

---
+++ aws_lambda_dockerfiles/mobilenet_v1/app.py
@@ -0,0 +1,19 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import time
+model_load_start_time = time.time()
+import tensorflow as tf
+model = tf.keras.models.load_model('./mobilenet_v1')
+model_load_end_time = time.time()
+
+def lambda_handler(event,context):
+    body = event['body-json']
+    start_time = time.time()
+    result = model.predict(body['inputs']['input_1'])
+    end_time = time.time()
+    return {
+        'statusCode': 200,
+        'loading_time': model_load_end_time - model_load_start_time,
+        'inference_time': end_time - start_time,
+        'body': result
+    }
