b55685407e80c2ea16a10eb1743df4e543f17612
mhsong <mhsong@kookmin.ac.kr>
2023-05-13 19:25:17 +0900

    Fix typo in dataset_download.sh

--- dataset/dataset_download.sh
+++ dataset/dataset_download.sh
@@ -1,17 +1,15 @@
 # #!/bin/bash
 
+#image classification image dataset (tfrecord imagenet)
+mkdir imagenet
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/imagenet_1000
+mv imagenet_1000 ./imagenet
 
-# #image classification image dataset (tfrecord imagenet)
-# mkdir imagenet
-# curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/imagenet_1000
-# mv imagenet_1000 ./imagenet
-
-# #image classification image dataset (raw imagenet)
-# curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/imagenet_metadata.txt
-# curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/imagenet_1000_raw.zip
-# mv imagenet_metadata.txt ./imagenet
-# unzip -q imagenet_1000_raw.zip -d ./imagenet && rm imagenet_1000_raw.zip
-
+#image classification image dataset (raw imagenet)
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/imagenet_metadata.txt
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/imagenet_1000_raw.zip
+mv imagenet_metadata.txt ./imagenet
+unzip -q imagenet_1000_raw.zip -d ./imagenet && rm imagenet_1000_raw.zip
 
 #object detection image dataset(coco_2017)
 mkdir coco_2017
