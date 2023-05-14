03cbe6c19d7cd91001d28af8c0f6d68294a1da81
mhsong <mhsong@kookmin.ac.kr>
2023-05-14 22:49:07 +0900

    Update preprocessing code : bert, distilbert_sst2

--- bench/bert_imdb/preprocessing.py
+++ bench/bert_imdb/preprocessing.py
@@ -2,12 +2,18 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
+from keras.preprocessing.sequence import pad_sequences
 
 def run_preprocessing(text):
     tokenizer = tf.keras.preprocessing.text.Tokenizer()
     tokenizer.fit_on_texts([text])
     input_ids = tokenizer.texts_to_sequences([text])
+    input_ids = pad_sequences(input_ids, maxlen=500, padding='post', truncating='post')
     input_masks = [[1] * len(input_ids[0])]
     segment_ids = [[0] * len(input_ids[0])]
-
+    print(input_ids)
+    print(input_masks)
+    print(segment_ids)
     return input_ids, input_masks, segment_ids 
+
+run_preprocessing("Test asdf asdf")
