955fd43c28b2beccb4319970c7f1d74ac3d6f685
mhsong <mhsong@kookmin.ac.kr>
2023-05-15 00:57:52 +0900

    Change directory structure

--- IaC/GCP-CloudRun-IaC/cloudrun/cloudrun.tf
+++ IaC/GCP-CloudRun-IaC/cloudrun/cloudrun.tf
@@ -5,7 +5,7 @@ variable "APIS" {
 
 resource "google_cloud_run_service" "cloudrun_service" {
   count    = 2
-  name     = "${replace(var.model_name,"_","-")}-${var.APIS[count.index]}"
+  name     = "cloud-run-${replace(var.model_name,"_","-")}-${var.APIS[count.index]}"
   location = var.region
   template {
     spec {
