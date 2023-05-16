daa8f12e12725ee1e1ba37519b1a7d26ca0481aa
mhsong <mhsong@kookmin.ac.kr>
2023-05-16 21:06:55 +0900

    Fix typo

--- bench/run_lambda_bench.py
+++ bench/run_lambda_bench.py
@@ -2,7 +2,7 @@ import lambda_variables
 import run_rest_bench
 import time
 
-def start_bench(model_names, num_tasks, lambda_address_default, spreadsheet_id):
+def start_bench(model_names, num_tasks, lambda_address_default, spreadsheet_id, bucket_name=''):
   for i, model_name in enumerate(model_names):
     for k, num_task in enumerate(num_tasks):
       run_rest_bench.main(model_name,
@@ -10,10 +10,12 @@ def start_bench(model_names, num_tasks, lambda_address_default, spreadsheet_id):
                           f"https://{model_name.replace('_','-')}.{lambda_address_default}/",
                           spreadsheet_id,
                           f"{model_name}-{num_task}",
-                          using_lambda=1)
+                          using_lambda=1,
+                          bucket_name=bucket_name)
       time.sleep(5)
 
 start_bench(lambda_variables.model_names,
      lambda_variables.num_tasks,
      lambda_variables.lambda_address_default,
-     lambda_variables.spreadsheet_id)
+     lambda_variables.spreadsheet_id,
+     lambda_variables.bucket_name)
