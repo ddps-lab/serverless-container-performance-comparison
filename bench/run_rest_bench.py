99b0738908afcece6c8fec706caa8a14c09c5515
mhsong <mhsong@kookmin.ac.kr>
2023-05-17 20:18:01 +0900

    bug fix

--- bench/run_rest_bench.py
+++ bench/run_rest_bench.py
@@ -7,7 +7,10 @@ rest_bench = ""
 def main(model_name, num_tasks, server_address, spreadsheet_id, worksheet_name, using_lambda=0, bucket_name=''):
   global rest_bench
   rest_bench = importlib.import_module(f"{model_name}.rest_bench")
-  result = rest_bench.run_bench(num_tasks, server_address, using_lambda=using_lambda, bucket_name=bucket_name)
+  if (using_lambda == 1 and bucket_name != ''):
+    result = rest_bench.run_bench(num_tasks, server_address, using_lambda=using_lambda, bucket_name=bucket_name)
+  else:
+    result = rest_bench.run_bench(num_tasks, server_address)
   put_data_into_sheet.put_data(spreadsheet_id, worksheet_name, result, num_tasks)
 
 if (__name__ == "__main__"):
