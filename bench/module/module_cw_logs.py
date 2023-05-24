a70775c23d271b83eb20513f09169f4a3c5330d3
mhsong <mhsong@kookmin.ac.kr>
2023-05-24 22:01:38 +0900

    Change cwlogs logging format

--- bench/module/module_cw_logs.py
+++ bench/module/module_cw_logs.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 import time
 
 logs_client = boto3.client('logs')
@@ -6,9 +7,13 @@ logs_client = boto3.client('logs')
 def create_log_stream(log_group_name, log_stream_name):
     logs_client.create_log_stream(logGroupName=log_group_name, logStreamName=log_stream_name)
 
-def create_log_event(log_group_name, log_stream_name, log_event_data):
+def create_log_event(log_group_name, log_stream_name, log_event_index, log_event_data):
+    log_data = {
+        'index_number': log_event_index,
+        'elapsed_time': log_event_data
+    }
     log_event = {
         'timestamp':int (time.time() * 1000),
-        'message': str(log_event_data)
+        'message': json.dumps(log_data)
     }
     logs_client.put_log_events(logGroupName=log_group_name, logStreamName=log_stream_name, logEvents=[log_event])
--- bench/module/run_faas_bench.py
+++ bench/module/run_faas_bench.py
@@ -13,8 +13,8 @@ def main(model_name, num_tasks, server_address, spreadsheet_id, worksheet_name, 
   else:
     inference_times, network_latency_times = faas_bench.run_bench(num_tasks, server_address)
   for i in range(len(inference_times)):
-    module_cw_logs.create_log_event(log_group_name, inference_time_log_stream_name, inference_times[i])
+    module_cw_logs.create_log_event(log_group_name, inference_time_log_stream_name, i+1, inference_times[i])
   for i in range(len(network_latency_times)):
-    module_cw_logs.create_log_event(log_group_name, network_latency_time_log_stream_name, network_latency_times[i])
+    module_cw_logs.create_log_event(log_group_name, network_latency_time_log_stream_name, i+1, network_latency_times[i])
   
   #put_data_into_sheet.put_data(spreadsheet_id, worksheet_name, result, num_tasks)
