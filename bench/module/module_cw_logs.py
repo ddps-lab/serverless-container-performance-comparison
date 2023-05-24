4d8b3c222297881c3acc6ffa94f0503b37159a41
mhsong <mhsong@kookmin.ac.kr>
2023-05-24 20:38:50 +0900

    Fix error in cloudwatch logs module

--- bench/module/module_cw_logs.py
+++ bench/module/module_cw_logs.py
@@ -9,6 +9,6 @@ def create_log_stream(log_group_name, log_stream_name):
 def create_log_event(log_group_name, log_stream_name, log_event_data):
     log_event = {
         'timestamp':int (time.time() * 1000),
-        'message': log_event_data
+        'message': str(log_event_data)
     }
     logs_client.put_log_events(logGroupName=log_group_name, logStreamName=log_stream_name, logEvents=[log_event])
