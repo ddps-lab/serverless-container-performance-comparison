35dfd8d43630055deeff437893f4b77d0f99ed8c
mhsong <mhsong@kookmin.ac.kr>
2023-05-25 15:50:54 +0900

    Change bench iac code to install awscli on userdata

--- bench/bench_azure_infra/script.sh
+++ bench/bench_azure_infra/script.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev liblzma-dev unzip curl docker.io
+xz-utils tk-dev liblzma-dev unzip curl docker.io awscli
 
 sudo echo 'export PYENV_ROOT="/pyenv"' >> /etc/bash.bashrc
 sudo echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> /etc/bash.bashrc
