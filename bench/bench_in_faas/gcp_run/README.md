58be12d6d64fda2eab4c6b1b4f224b24d8f7e9c5
mhsong <mhsong@kookmin.ac.kr>
2023-05-29 16:00:33 +0900

    Add GCP Run bench in faas

---
+++ bench/bench_in_faas/gcp_run/README.md
@@ -0,0 +1,1 @@
+This is GCP Functions code to bench GCP Run.
---
+++ bench/bench_in_faas/gcp_run/build_assets/main.py
@@ -0,0 +1,62 @@
+import functions_framework
+import json
+import boto3
+import time
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+
+import tensorflow as tf
+from google.protobuf.json_format import Parse
+import grpc
+
+access_key = os.environ['AWS_ACCESS_KEY_ID']
+secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
+
+def create_grpc_stub(server_address, use_https):
+    # gRPC 채널 생성
+    if (use_https == "1"):
+        channel = grpc.secure_channel(server_address,grpc.ssl_channel_credentials(),options=[('grpc.max_send_message_length', 50 * 1024 * 1024), ('grpc.max_receive_message_length', 50 * 1024 * 1024)])
+    else:
+        channel = grpc.insecure_channel(server_address,options=[('grpc.max_send_message_length', 50 * 1024 * 1024), ('grpc.max_receive_message_length', 50 * 1024 * 1024)])
+
+    # gRPC 스텁 생성
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+    return stub
+
+def predict(stub, data):
+    request_time = time.time()
+    response = stub.Predict(data, timeout=100.0)
+    response_time = time.time()
+    network_latency_time = response_time - request_time
+    return response, network_latency_time
+
+def create_log_event(log_group_name, log_stream_name, inference_time, network_latency_time):
+    logs_client = boto3.client('logs', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+    log_data = {
+        'inference_time': inference_time,
+        'network_latency_time': network_latency_time
+    }
+    log_event = {
+        'timestamp':int (time.time() * 1000),
+        'message': json.dumps(log_data)
+    }
+    logs_client.put_log_events(logGroupName=log_group_name, logStreamName=log_stream_name, logEvents=[log_event])
+
+@functions_framework.http
+def function_handler(request):
+    if request.method == 'POST':
+        json_body = request.get_json(silent=True)
+        log_group_name = json_body['inputs']['log_group_name']
+        log_stream_name = json_body['inputs']['log_stream_name']
+        server_address = json_body['inputs']['server_address']
+        use_https = json_body['inputs']['use_https']
+        request_data = Parse(json_body['inputs']['request_data'])
+        stub = create_grpc_stub(server_address, use_https)
+        response, network_latency_time = predict(stub, request_data)
+        create_log_event(log_group_name, log_stream_name, "null", network_latency_time)
+        return json.dumps({'body': "Success"}), 200, {'Content-Type': 'application/json'}
+    else:
+        return json.dumps({
+            'body': "Please send POST request"
+        }), 403, {'Content-Type': 'application/json'}
