0ce3cce0658d5cd3e94eec5babb8179ed89095af
mhsong <mhsong@kookmin.ac.kr>
2023-06-22 00:19:49 +0900

    Update bench app code

--- bench/bench_in_faas/aws_sagemaker/build_assets/app.py
+++ bench/bench_in_faas/aws_sagemaker/build_assets/app.py
@@ -2,21 +2,29 @@ import json
 import requests
 import boto3
 import time
+from sagemaker import Session
+from sagemaker.predictor import Predictor
 
-def predict(server_address, data):
-    headers = {"content-type": "application/json"}
-    url = server_address
+def predict(sagemaker_endpoint, data):
+    session = Session()
+    predictor = Predictor(endpoint_name=sagemaker_endpoint, sagemaker_session=session)    
     request_time = time.time()
-    response = requests.post(url, data=data, headers=headers)
+    response = predictor.predict(data)
     response_time = time.time()
     network_latency_time = response_time - request_time
-    result = response.text
-    return result, network_latency_time
+    return response, network_latency_time
 
-def create_log_event(log_group_name, log_stream_name, network_latency_time):
+def create_log_event(log_group_name, log_stream_name, result, network_latency_time):
+    result_json = json.loads(result)
     logs_client = boto3.client('logs')
     log_data = {
-        'network_latency_time': network_latency_time
+        'inference_time': result['inference_time'],
+        'network_latency_time': network_latency_time,
+        'cpu_info': result['cpu_info'],
+        'mem_info': result['mem_info'],
+        'num_cores': result['num_cores'],
+        'mem_bytes': result['mem_bytes'],
+        'mem_gib': result['mem_gib']
     }
     log_event = {
         'timestamp':int (time.time() * 1000),
@@ -32,9 +40,9 @@ def lambda_handler(event,context):
     log_stream_name = json_body['inputs']['log_stream_name']
     aws_sagemaker_endpoint_prefix = json_body['inputs']['sagemaker_endpoint_prefix']
     request_data = json_body['inputs']['request_data']
-    server_address = f"https://runtime.sagemaker.{aws_region}.amazonaws.com/endpoints/{aws_sagemaker_endpoint_prefix}-{model_name.replace('_','-')}-endpoint/invocations"
-    result, network_latency_time = predict(server_address, request_data)
-    create_log_event(log_group_name, log_stream_name, network_latency_time)
+    sagemaker_endpoint = f"{aws_sagemaker_endpoint_prefix}-{model_name.replace('_','-')}-endpoint"
+    result, network_latency_time = predict(sagemaker_endpoint, request_data)
+    create_log_event(log_group_name, log_stream_name, result, network_latency_time)
     response = {
         'statusCode': 200,
         'body': "Success"
