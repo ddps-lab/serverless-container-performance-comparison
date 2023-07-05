0f491131ec8f6edfdfa5265e7d4f8a367256de4e
Moohyun Song <crewnadm@gmail.com>
2023-07-05 08:13:14 +0000

    Add bench execute time check code for AWS Lambda, Sagemaker

--- bench/bench_in_faas/aws_lambda/build_assets/app.py
+++ bench/bench_in_faas/aws_lambda/build_assets/app.py
@@ -13,9 +13,10 @@ def predict(server_address, data):
     result = response.json()
     return result, network_latency_time
 
-def create_log_event(log_group_name, log_stream_name, start_latency_time, result, network_latency_time):
+def create_log_event(log_group_name, log_stream_name, start_latency_time, result, network_latency_time, bench_execute_latency_time):
     logs_client = boto3.client('logs')
     log_data = {
+        'bench_execute_latency_time': bench_execute_latency_time,
         'start_latency_time': start_latency_time,
         'inference_time': result['inference_time'],
         'network_latency_time': network_latency_time,
@@ -32,6 +33,7 @@ def create_log_event(log_group_name, log_stream_name, start_latency_time, result
     logs_client.put_log_events(logGroupName=log_group_name, logStreamName=log_stream_name, logEvents=[log_event])
 
 def lambda_handler(event,context):
+    bench_execute_time = time.time()
     json_body = json.loads(event['body'])
     model_name = json_body['inputs']['model_name']
     log_group_name = json_body['inputs']['log_group_name']
@@ -40,6 +42,7 @@ def lambda_handler(event,context):
     request_data = json_body['inputs']['request_data']
     bucket_name = json_body['inputs']['bucket_name']
     upload_time = int(json_body['inputs']['upload_time'])
+    bench_execute_request_time = json_body['inputs']['bench_execute_request_time']
     download_time = 0
     request_time = time.time()
     result, network_latency_time = predict(server_address, request_data)
@@ -48,7 +51,7 @@ def lambda_handler(event,context):
         download_start_time = time.time()
         s3_resource.Bucket(bucket_name).download_file("predict_data.npy", "/tmp/tmp_file")
         download_time = time.time() - download_start_time
-    create_log_event(log_group_name, log_stream_name, request_time - result['start_time'], result, upload_time+network_latency_time+download_time)
+    create_log_event(log_group_name, log_stream_name, result['start_time'] - request_time, result, upload_time+network_latency_time+download_time, bench_execute_time - bench_execute_request_time)
     response = {
         'statusCode': 200,
         'body': "Success"
--- bench/bench_in_faas/aws_sagemaker/build_assets/app.py
+++ bench/bench_in_faas/aws_sagemaker/build_assets/app.py
@@ -14,10 +14,11 @@ def predict(sagemaker_endpoint, data, bucket_name):
     network_latency_time = response_time - request_time
     return response, network_latency_time
 
-def create_log_event(log_group_name, log_stream_name, request_time, result, network_latency_time):
+def create_log_event(log_group_name, log_stream_name, request_time, result, network_latency_time, bench_execute_latency_time):
     result_json = json.loads(result)
     logs_client = boto3.client('logs')
     log_data = {
+        'bench_execute_latency_time': bench_execute_latency_time,
         'start_latency_time': result_json['start_time'] - request_time,
         'inference_time': result_json['inference_time'],
         'network_latency_time': network_latency_time,
@@ -34,6 +35,7 @@ def create_log_event(log_group_name, log_stream_name, request_time, result, netw
     logs_client.put_log_events(logGroupName=log_group_name, logStreamName=log_stream_name, logEvents=[log_event])
 
 def lambda_handler(event,context):
+    bench_execute_time = time.time()
     json_body = json.loads(event['body'])
     aws_region = json_body['inputs']['aws_region']
     model_name = json_body['inputs']['model_name']
@@ -43,6 +45,7 @@ def lambda_handler(event,context):
     request_data = json_body['inputs']['request_data']
     upload_time = json_body['inputs']['upload_time']
     bucket_name = json_body['inputs']['bucket_name']
+    bench_execute_request_time = json_body['inputs']['bench_execute_request_time']
     sagemaker_endpoint = f"{aws_sagemaker_endpoint_prefix}-{model_name.replace('_','-')}-endpoint"
     request_time = time.time()
     result, network_latency_time = predict(sagemaker_endpoint, request_data, bucket_name)
@@ -52,7 +55,7 @@ def lambda_handler(event,context):
         download_start_time = time.time()
         s3_resource.Bucket(bucket_name).download_file("predict_data.npy", "/tmp/tmp_file")
         download_time = time.time() - download_start_time
-    create_log_event(log_group_name, log_stream_name, request_time, result, upload_time+network_latency_time+download_time)
+    create_log_event(log_group_name, log_stream_name, request_time, result, upload_time+network_latency_time+download_time, bench_execute_time - bench_execute_request_time)
     response = {
         'statusCode': 200,
         'body': "Success"
