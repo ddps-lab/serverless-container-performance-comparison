06937385073f65c3eb692c18c589befd155cc26d
Moohyun Song <crewnadm@gmail.com>
2023-07-07 11:04:11 +0000

    Update bench request code to golang (aws_sagemaker)

--- bench/bench_in_faas/aws_sagemaker/build_assets/app.py
+++ bench/bench_in_faas/aws_sagemaker/build_assets/app.py
@@ -5,7 +5,7 @@ import time
 from sagemaker import Session
 from sagemaker.predictor import Predictor
 
-def predict(sagemaker_endpoint, data, bucket_name):
+def predict(sagemaker_endpoint, data):
     session = Session()
     predictor = Predictor(endpoint_name=sagemaker_endpoint, sagemaker_session=session)    
     request_time = time.time()
@@ -37,25 +37,29 @@ def create_log_event(log_group_name, log_stream_name, request_time, result, netw
 def lambda_handler(event,context):
     bench_execute_time = time.time()
     json_body = json.loads(event['body'])
-    aws_region = json_body['inputs']['aws_region']
+    # aws_region = json_body['inputs']['aws_region']
     model_name = json_body['inputs']['model_name']
     log_group_name = json_body['inputs']['log_group_name']
     log_stream_name = json_body['inputs']['log_stream_name']
     aws_sagemaker_endpoint_prefix = json_body['inputs']['sagemaker_endpoint_prefix']
-    request_data = json_body['inputs']['request_data']
-    upload_time = json_body['inputs']['upload_time']
-    bucket_name = json_body['inputs']['bucket_name']
+    # request_data = json_body['inputs']['request_data']
+    # upload_time = json_body['inputs']['upload_time']
+    # bucket_name = json_body['inputs']['bucket_name']
     bench_execute_request_time = json_body['inputs']['bench_execute_request_time']
+    with open(f"./{model_name}.json", "r", encoding="utf-8") as f:
+        request_data = json.dumps(json.load(f))
     sagemaker_endpoint = f"{aws_sagemaker_endpoint_prefix}-{model_name.replace('_','-')}-endpoint"
     request_time = time.time()
-    result, network_latency_time = predict(sagemaker_endpoint, request_data, bucket_name)
-    download_time = 0
-    if (model_name == "yolo_v5" or model_name == "inception_v3"):
-        s3_resource = boto3.resource('s3')
-        download_start_time = time.time()
-        s3_resource.Bucket(bucket_name).download_file("predict_data.npy", "/tmp/tmp_file")
-        download_time = time.time() - download_start_time
-    create_log_event(log_group_name, log_stream_name, request_time, result, upload_time+network_latency_time+download_time, bench_execute_time - bench_execute_request_time)
+    # result, network_latency_time = predict(sagemaker_endpoint, request_data, bucket_name)
+    result, network_latency_time = predict(sagemaker_endpoint, request_data)
+    # download_time = 0
+    # if (model_name == "yolo_v5" or model_name == "inception_v3"):
+    #     s3_resource = boto3.resource('s3')
+    #     download_start_time = time.time()
+    #     s3_resource.Bucket(bucket_name).download_file("predict_data.npy", "/tmp/tmp_file")
+    #     download_time = time.time() - download_start_time
+    # create_log_event(log_group_name, log_stream_name, request_time, result, upload_time+network_latency_time+download_time, bench_execute_time - bench_execute_request_time)
+    create_log_event(log_group_name, log_stream_name, request_time, result, network_latency_time, bench_execute_time - bench_execute_request_time)
     response = {
         'statusCode': 200,
         'body': "Success"
---
+++ bench/bench_in_faas/aws_sagemaker/run_bench.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ./variables.sh
+
+# warm bench request lambda instances
+go run run_bench.go --model_name "unknown" --task_num 100 --log_group_name "unknown" --server_address $server_address --aws_sagemaker_endpoint_prefix "unknown"
+sleep 5
+go run run_bench.go --model_name "unknown" --task_num 100 --log_group_name "unknown" --server_address $server_address --aws_sagemaker_endpoint_prefix "unknown"
+sleep 5
+for model_name in "${model_names[@]}"
+do
+  for task_num in "${task_nums[@]}"
+  do
+    go run run_bench.go --model_name $model_name --task_num $task_num --log_group_name $log_group_name --server_address $server_address --aws_sagemaker_endpoint_prefix $aws_sagemaker_endpoint_prefix
+    sleep 5
+  done
+done
