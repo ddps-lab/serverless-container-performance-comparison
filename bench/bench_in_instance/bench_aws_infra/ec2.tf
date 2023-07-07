6b79f77cedb8ba1a47ddba8b78e8d4edd250d254
mhsong <mhsong@kookmin.ac.kr>
2023-07-08 00:44:20 +0900

    Update aws ec2 bench infra userdata

--- bench/bench_in_instance/bench_aws_infra/ec2.tf
+++ bench/bench_in_instance/bench_aws_infra/ec2.tf
@@ -53,10 +53,7 @@ resource "aws_instance" "bench_instance" {
   user_data = <<-EOF
   #!/bin/bash
   apt update
-  apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev liblzma-dev unzip curl docker.io awscli
-
+  apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev unzip curl docker.io awscli
   echo 'export PYENV_ROOT="/pyenv"' >> /etc/bash.bashrc
   echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> /etc/bash.bashrc
   echo 'eval "$(pyenv init -)"' >> /etc/bash.bashrc
@@ -64,7 +61,7 @@ xz-utils tk-dev liblzma-dev unzip curl docker.io awscli
   git clone https://github.com/pyenv/pyenv.git /pyenv
   source /etc/bash.bashrc
 
-  pyenv install 3.10.11
+  /pyenv/bin/pyenv install 3.10.11
   chmod 777 -R /pyenv
   EOF
   root_block_device {
