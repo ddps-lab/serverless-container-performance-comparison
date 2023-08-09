9cc09f9a04907d420dc0b40e9d233bf061100632
mhsong <mhsong@kookmin.ac.kr>
2023-08-09 23:25:19 +0900

    Fix typo in bench_code

--- bench/bench_in_instance/run_gcp_run_rest_bench.py
+++ bench/bench_in_instance/run_gcp_run_rest_bench.py
@@ -10,7 +10,7 @@ def start_bench(model_names, num_tasks, gcp_run_default_address, spreadsheet_id)
                           f"https://{model_name.replace('_','-')}-rest-{gcp_run_default_address}/",
                           spreadsheet_id,
                           f"{model_name}-rest-{num_task}")
-      time.sleep(5)
+      time.sleep(30)
 
 start_bench(gcp_run_variables.model_names,
      gcp_run_variables.num_tasks,
