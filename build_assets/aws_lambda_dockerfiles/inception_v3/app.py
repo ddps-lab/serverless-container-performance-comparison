1526ce6bcbea62c23c2db0e5d6f65e7ca6fa3d81
mhsong <mhsong@kookmin.ac.kr>
2023-09-02 02:20:25 +0900

    Change aws lamba inference code about unexpected inference time

--- build_assets/aws_lambda_dockerfiles/inception_v3/app.py
+++ build_assets/aws_lambda_dockerfiles/inception_v3/app.py
@@ -18,8 +18,9 @@ def lambda_handler(event,context):
     execution_start_time = time.time()
     json_body = json.loads(event['body'])
     input_3 = json_body['inputs']['input_3']
+    data = np.array(input_3)
     inference_start_time = time.time()
-    result = model.predict(np.array(input_3))
+    result = model.predict(data)
     inference_end_time = time.time()
     mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
     mem_gib = mem_bytes/(1024.**3)
--- build_assets/aws_lambda_dockerfiles/mobilenet_v2/app.py
+++ build_assets/aws_lambda_dockerfiles/mobilenet_v2/app.py
@@ -18,8 +18,9 @@ def lambda_handler(event,context):
     execution_start_time = time.time()
     json_body = json.loads(event['body'])
     input_2 = json_body['inputs']['input_2']
+    data = np.array(input_2)
     inference_start_time = time.time()
-    result = model.predict(np.array(input_2))
+    result = model.predict(data)
     inference_end_time = time.time()
     mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
     mem_gib = mem_bytes/(1024.**3)
