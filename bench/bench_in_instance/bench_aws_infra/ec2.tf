b2e22cb22053616b5e15ced4be4a485843f29187
mh3ong <mhsong@kookmin.ac.kr>
2024-01-31 04:06:13 +0000

    Add package : docker-buildx

--- bench/bench_in_instance/bench_aws_infra/ec2.tf
+++ bench/bench_in_instance/bench_aws_infra/ec2.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "bench_instance" {
   user_data = <<-EOF
   #!/bin/bash
   apt update
-  apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev unzip curl docker.io awscli
+  apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev unzip curl docker.io awscli docker-buildx
   echo 'export PYENV_ROOT="/pyenv"' >> /etc/bash.bashrc
   echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> /etc/bash.bashrc
   echo 'eval "$(pyenv init -)"' >> /etc/bash.bashrc
